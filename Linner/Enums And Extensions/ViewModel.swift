bd5e8f2641d2432cfcc4eb1ddebaaff7dc0afc86
Iram Fattah <iramfattah94@gmail.com>
2019-01-22 16:03:05 -0500

    added unit tests for data

---
+++ Linner/Enums And Extensions/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  Linner
+//
+//  Created by C4Q on 1/22/19.
+//  Copyright © 2019 Iram Fattah. All rights reserved.
+//
+
+import Foundation
+protocol ViewModel {}
+
+// These extensions are for testing purposes.
+extension RecipesViewModel: ViewModel {}
+extension MenusViewModel: ViewModel {}
